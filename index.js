5e43f679fc695f01aad7fced94de0f86edb0f268
UnicornL-NgocLan <lengoclan8a22002@gmail.com>
2024-10-14 16:21:48 +0700

    add hpp

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const hpp = require('hpp');
 
 const whitelist = [
   "http://localhost:5000",
@@ -40,6 +41,7 @@ app.use(bodyParser.json({limit: '1mb', type: 'application/json'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: "100kb" }));
 app.use(cors());
 app.use(helmet());
+app.use(hpp());
 app.use(morgan('tiny'));
 
 
