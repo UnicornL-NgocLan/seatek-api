fd9b61137a00c3df837b3e54eb9ca045f8b819b2
UnicornL-NgocLan <lengoclan8a22002@gmail.com>
2025-08-06 13:27:12 +0700

    Add Puppeteer dependency and enhance file processing routes
    - Added Puppeteer as a dependency in package.json for PDF manipulation.
    - Refactored fileProcessRoute.js to improve code readability and maintainability.
    - Introduced a new route for exporting PDF from Metabase.

--- server/controllers/fileProcess.js
+++ server/controllers/fileProcess.js
@@ -1,35 +1,94 @@
-const pdfParse = require("pdf-parse");
-const { extractPdfImageFromOCR } = require("../utils/ocrProcess.js");
-const fs = require("fs");
+const pdfParse = require('pdf-parse')
+const { extractPdfImageFromOCR } = require('../utils/ocrProcess.js')
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+
+const metabaseTodaySaleUrl = process.env.TODAY_SALE_METABASE_DASHBOARD
 
 const fileProcessCtrl = {
-  extractTextFromPdf: async (req, res) => {
-    try {
-      if (!req.file) return res.status(400).send("No file uploaded!");
-      let result = "";
-      // Extract text from PDF buffer
-      const buffer = fs.readFileSync(req.file.path);
-      const data = await pdfParse(buffer);
-      if (data.text.trim()) {
-        result = data.text;
-      } else {
-        result = await extractPdfImageFromOCR(buffer);
-      }
-      // Send extracted text as response
-      res.send({
-        extractedText: result,
-      });
-    } catch (error) {
-      res.status(500).json({ msg: error.message });
-    } finally {
-      if (req.file) {
-        fs.unlink(req.file.path, (err) => {
-          if (err) console.error("Failed to delete uploaded file:", err);
-          else console.log("Temporary file deleted.");
-        });
-      }
-    }
-  },
-};
+    extractTextFromPdf: async (req, res) => {
+        try {
+            if (!req.file) return res.status(400).send('No file uploaded!')
+            let result = ''
+            // Extract text from PDF buffer
+            const buffer = fs.readFileSync(req.file.path)
+            const data = await pdfParse(buffer)
+            if (data.text.trim()) {
+                result = data.text
+            } else {
+                result = await extractPdfImageFromOCR(buffer)
+            }
+            // Send extracted text as response
+            res.send({
+                extractedText: result,
+            })
+        } catch (error) {
+            res.status(500).json({ msg: error.message })
+        } finally {
+            if (req.file) {
+                fs.unlink(req.file.path, (err) => {
+                    if (err)
+                        console.error('Failed to delete uploaded file:', err)
+                    else console.log('Temporary file deleted.')
+                })
+            }
+        }
+    },
 
-module.exports = fileProcessCtrl;
+    exportPdfFromMetabase: async (req, res) => {
+        const { code } = req.query
+        let myRespectiveUrl = ''
+
+        switch (code) {
+            case 'todaysale':
+                myRespectiveUrl = metabaseTodaySaleUrl
+                break
+            default:
+                return res.status(400).send('Invalid code')
+        }
+
+        let browser
+        try {
+            browser = await puppeteer.launch({
+                headless: 'new',
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            })
+
+            const page = await browser.newPage()
+            await page.goto(myRespectiveUrl, { waitUntil: 'networkidle0' })
+
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+
+            const pdfBuffer = await page.pdf({
+                format: 'A4',
+                printBackground: true,
+                landscape: true,
+                margin: {
+                    top: '20px',
+                    bottom: '20px',
+                    left: '20px',
+                    right: '20px',
+                },
+            })
+
+            console.log('URL:', myRespectiveUrl)
+            console.log('PDF size:', pdfBuffer.length)
+
+            await browser.close()
+
+            res.set({
+                'Content-Type': 'application/pdf',
+                'Content-Disposition': 'inline; filename="dashboard.pdf"',
+                'Content-Length': pdfBuffer.length,
+            })
+
+            return res.end(pdfBuffer) // <-- Fix chỗ này
+        } catch (error) {
+            if (browser) await browser.close()
+            console.error('Lỗi khi tạo PDF:', error)
+            return res.status(500).send('Lỗi khi tạo PDF')
+        }
+    },
+}
+
+module.exports = fileProcessCtrl
--- server/routes/fileProcessRoute.js
+++ server/routes/fileProcessRoute.js
@@ -1,32 +1,38 @@
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-const path = require("path");
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const path = require('path')
 
 const {
-  authenticateEmployeeKey,
-} = require("../middlewares/checkForEmployeeApiKey");
-const fileProcessCtrl = require("../controllers/fileProcess");
+    authenticateEmployeeKey,
+} = require('../middlewares/checkForEmployeeApiKey')
+const fileProcessCtrl = require('../controllers/fileProcess')
 
 // configure disk storage
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // make sure 'uploads/' folder exists
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
-});
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/') // make sure 'uploads/' folder exists
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+        cb(null, uniqueSuffix + path.extname(file.originalname))
+    },
+})
 
 // initialize upload middleware
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage })
 
 router.post(
-  "/get-extract-pdf-data",
-  authenticateEmployeeKey,
-  upload.single("file"),
-  fileProcessCtrl.extractTextFromPdf
-);
+    '/get-extract-pdf-data',
+    authenticateEmployeeKey,
+    upload.single('file'),
+    fileProcessCtrl.extractTextFromPdf
+)
 
-module.exports = router;
+router.get(
+    '/get-metabase-pdf-dashboard',
+    authenticateEmployeeKey,
+    fileProcessCtrl.exportPdfFromMetabase
+)
+
+module.exports = router
