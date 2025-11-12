c2e99b5c9c791c43df81c0d62a0371e0b2c49003
UnicornL-NgocLan <lengoclan8a22002@gmail.com>
2025-11-12 09:29:11 +0700

    feat: update Odoo connection to support test environment configuration

--- server/middlewares/authorizeOdoo.js
+++ server/middlewares/authorizeOdoo.js
@@ -2,30 +2,43 @@ const Odoo = require('odoo-xmlrpc')
 
 const odooAuthorize = async (req, res, next) => {
     try {
+        const isTest = req.query.is_test === 'true'
         const odoo = new Odoo({
-            url: process.env.ODOO_HOST,
-            db: process.env.ODOO_DATABASE,
+            url: isTest ? process.env.ODOO_TEST_HOST : process.env.ODOO_HOST,
+            db: isTest
+                ? process.env.ODOO_TEST_DATABASE
+                : process.env.ODOO_DATABASE,
             username: process.env.ODOO_USERNAME,
             password: process.env.ODOO_PASSWORD,
         })
 
         const odoo_retail = new Odoo({
-            url: process.env.ODOO_RETAIL_HOST,
-            db: process.env.ODOO_RETAIL_DATABASE,
+            url: isTest
+                ? process.env.ODOO_TEST_RETAIL_HOST
+                : process.env.ODOO_RETAIL_HOST,
+            db: isTest
+                ? process.env.ODOO_TEST_RETAIL_DATABASE
+                : process.env.ODOO_RETAIL_DATABASE,
             username: process.env.ODOO_RETAIL_USERNAME,
             password: process.env.ODOO_RETAIL_PASSWORD,
         })
 
         const odoo_pos_tool = new Odoo({
-            url: process.env.ODOO_HOST,
-            db: process.env.ODOO_DATABASE,
+            url: isTest ? process.env.ODOO_TEST_HOST : process.env.ODOO_HOST,
+            db: isTest
+                ? process.env.ODOO_TEST_DATABASE
+                : process.env.ODOO_DATABASE,
             username: process.env.ODOO_POS_TOOL_USERNAME,
             password: process.env.ODOO_POS_TOOL_PASSWORD,
         })
 
         const odoo_retail_pos_tool = new Odoo({
-            url: process.env.ODOO_RETAIL_HOST,
-            db: process.env.ODOO_RETAIL_DATABASE,
+            url: isTest
+                ? process.env.ODOO_TEST_RETAIL_HOST
+                : process.env.ODOO_RETAIL_HOST,
+            db: isTest
+                ? process.env.ODOO_TEST_RETAIL_DATABASE
+                : process.env.ODOO_RETAIL_DATABASE,
             username: process.env.ODOO_RETAIL_POS_TOOL_USERNAME,
             password: process.env.ODOO_POS_TOOL_PASSWORD,
         })
